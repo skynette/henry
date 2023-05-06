a41d10758648ce26b3678308adfae3605f00d549
Joshua <cutejosh2@gmail.com>
2023-05-06 17:06:13 +0100

    register page done

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -2,17 +2,17 @@ import googleImage from '../assets/google.png';
 
 function Register() {
 	return (
-		<div className="flex flex-col md:flex-row min-h-screen">
+		<div className="flex flex-col md:flex-row min-h-screen bg-registerBg">
 			{/* Left section */}
 			<div className="register-left-section md:w-2/3 flex justify-center items-center">
 				<div className="text-center">
-					<h1 className="text-4xl font-bold text-gray-800 mb-4">Brand Name</h1>
-					<p className="text-lg text-gray-600">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+					<h1 className="text-4xl font-bold text-white mb-4">Brand Name</h1>
+					<p className="text-lg text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
 				</div>
 			</div>
 
 			{/* Right section */}
-			<div className="md:w-1/2 bg-registerBg flex flex-col justify-center m-auto items-center">
+			<div className="md:w-1/2 flex flex-col justify-center m-auto mt-32 items-center">
 				<form className="w-full max-w-md px-14">
 					<h2 className="text-3xl text-center text-gray-800 mb-8 py-6">Create Account</h2>
 					<div className="mb-4">
