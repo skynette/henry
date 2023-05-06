b5840dab880d8f89c5ac8a33341d4714f2494c01
Joshua <cutejosh2@gmail.com>
2023-05-06 22:15:45 +0100

    service detail card

--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,11 +1,12 @@
 
-const ServiceCard = () => {
+// eslint-disable-next-line react/prop-types
+const ServiceCard = ({ name, image }) => {
 	return (
 		<div className="w-full md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-8">
 			<div className="bg-white rounded-lg shadow-lg overflow-hidden">
-				<img className="w-full h-48 object-cover" src="https://via.placeholder.com/640x360" alt="Service" />
+				<img className="w-full h-48 object-cover" src={image} alt="Service" />
 				<div className="p-4">
-					<h2 className="text-xl font-bold mb-2">Service Title</h2>
+					<h2 className="text-xl font-bold mb-2">{name}</h2>
 					<p className="text-gray-700 text-base mb-4">Service Description</p>
 					<div className="flex items-center mb-2">
 						<svg className="w-4 h-4 fill-current text-gray-500 mr-2" viewBox="0 0 20 20">
--- src/components/ServiceDetail.jsx
+++ src/components/ServiceDetail.jsx
@@ -1,6 +1,28 @@
 import { useState } from 'react';
 import ServiceCard from './ServiceCard';
 
+import foodimage from "../assets/food.png"
+import accessoriesImage from "../assets/accessories.png"
+import cleaningImage from "../assets/cleaning.png"
+import clothingImage from "../assets/clothing.png"
+import hairImage from "../assets/hair.png"
+import healthImage from "../assets/health.png"
+import homeImage from "../assets/home.png"
+import kidsImage from "../assets/kids.png"
+import personalCareImage from "../assets/personal_care.png"
+
+const imageList = [
+	{ image: foodimage, title: "FOOD" },
+	{ image: accessoriesImage, title: "ACCESSORIES" },
+	{ image: cleaningImage, title: "CLEANING" },
+	{ image: clothingImage, title: "CLOTHING" },
+	{ image: hairImage, title: "HAIR" },
+	{ image: healthImage, title: "HEALTH" },
+	{ image: homeImage, title: "HOME" },
+	{ image: kidsImage, title: "KIDS" },
+	{ image: personalCareImage, title: "PERSONAL CARE" },
+]
+
 const ServiceDetail = () => {
 	const [locationFilter, setLocationFilter] = useState('');
 	const [typeFilter, setTypeFilter] = useState('');
@@ -20,27 +42,24 @@ const ServiceDetail = () => {
 
 	return (
 		<div className="container mx-auto">
-			<div className="flex justify-between items-center my-4">
+			<div className="flex justify-between items-center my-4 px-4 gap-2">
 				<div className="w-1/3">
 					<label htmlFor="locationFilter" className="block font-bold mb-2">Location:</label>
-					<input type="text" id="locationFilter" name="locationFilter" value={locationFilter} onChange={handleLocationFilterChange} className="w-full border border-gray-400 p-2 rounded" />
+					<input type="text" id="locationFilter" placeholder='Location...' name="locationFilter" value={locationFilter} onChange={handleLocationFilterChange} className="w-full border border-gray-400 p-2 rounded" />
 				</div>
 				<div className="w-1/3">
 					<label htmlFor="typeFilter" className="block font-bold mb-2">Type:</label>
-					<input type="text" id="typeFilter" name="typeFilter" value={typeFilter} onChange={handleTypeFilterChange} className="w-full border border-gray-400 p-2 rounded" />
+					<input type="text" id="typeFilter" name="typeFilter" placeholder='type...' value={typeFilter} onChange={handleTypeFilterChange} className="w-full border border-gray-400 p-2 rounded" />
 				</div>
 				<div className="w-1/3">
 					<label htmlFor="nameFilter" className="block font-bold mb-2">Name:</label>
-					<input type="text" id="nameFilter" name="nameFilter" value={nameFilter} onChange={handleNameFilterChange} className="w-full border border-gray-400 p-2 rounded" />
+					<input type="text" id="nameFilter" name="nameFilter" placeholder='Name...' value={nameFilter} onChange={handleNameFilterChange} className="w-full border border-gray-400 p-2 rounded" />
 				</div>
 			</div>
-			<div className="flex flex-wrap -mx-4">
-				<ServiceCard />
-				<ServiceCard />
-				<ServiceCard />
-				<ServiceCard />
-				<ServiceCard />
-				<ServiceCard />
+			<div className="flex flex-wrap mx-auto">
+				{imageList.map((imageObj) => (
+					<ServiceCard key={imageObj.name} name={imageObj.title} image={imageObj.image} />
+				))}
 			</div>
 		</div>
 	);
