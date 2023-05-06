8a0438d86d880d9fb35bd8543f31bf5caccd1b60
Joshua <cutejosh2@gmail.com>
2023-05-06 14:05:07 +0100

    successfully changed font type

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-        "./index.html",
-        "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                'primary': '#F1DFCD',
+	content: [
+		"./index.html",
+		"./src/**/*.{js,ts,jsx,tsx}",
+	],
+	theme: {
+		fontFamily: {
+			sans: ['Book Antiqua', 'sans-serif'],
+			serif: ['Book Antiqua', 'serif'],
+			mono: ['Book Antiqua', 'monospace'],
+		},
+		extend: {
+			colors: {
+				'primary': '#F1DFCD',
 				'background': '#F5F0EF',
 				'accent': '#CAB8A2',
 				'footer': '#E8E1D8',
-            }
-        },
-    },
-    plugins: [],
+			},
+		},
+	},
+	plugins: [],
 }
